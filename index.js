49bc66be888cf57a14289cc35e65cd72c6fa3269
PeiYun <ollylee8520@gmail.com>
2023-12-13 18:33:30 -0700

    update frontend typeahead form

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $.typeahead({
+    input: ".js-typeahead",
+    hint: true,
+    maxLength: 8,
+    minLength: 1,
+    display: ["title"],
+    source: {
+      url: "https://bringx-us-state-server.onrender.com/states",
+    },
+    callback: {
+      onClickAfter: function (node, a, item, event) {
+        window.location.href = item.url;
+      },
+    },
+  });
+});
